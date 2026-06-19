97ec56410044882db3329c2c8c137733e418defa
yohey404 <yohey61041@gmail.com>
2026-06-19 12:25:52 +0900

    Update check.py

--- check.py
+++ check.py
@@ -1,15 +1,18 @@
-import requests
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
-    browser = p.chromium.launch()
-    page = browser.new_page()
+browser = p.chromium.launch(headless=True)
+page = browser.new_page()
 
-    page.goto(
-        "https://toto.rakuten.co.jp/big/carryover/",
-        wait_until="networkidle"
-    )
+```
+page.goto(
+    "https://toto.rakuten.co.jp/big/carryover/",
+    wait_until="networkidle"
+)
 
-    print(page.title())
+text = page.locator("body").inner_text()
 
-    browser.close()
+print(text[:5000])
+
+browser.close()
+```
